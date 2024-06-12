edb015594f698e5147184129ef043369f5ef2e5c
ratigrdzelidze <164761160+ratigrdzelidze@users.noreply.github.com>
2024-06-12 17:37:27 +0400

    Add files via upload

---
+++ leqcia19/script.js
@@ -0,0 +1,143 @@
+// let myname = "rati"
+// let myhobby = "play games, watch movies, play football"
+// let yearofbirth = 2008
+// let currentyear = 2024
+
+// console.log('my name is' + myname + "my hobby is" + myhobby + "my year of birth is" + yearofbirth + "current year is" + currentyear)
+
+
+
+
+
+// let ricxvi =[5, 10, 15, 20, 25]
+// console.log(ricxvi[0] + ricxvi[1] + ricxvi[2] + ricxvi[3] + ricxvi[4]/5)
+
+
+ 
+
+
+ 
+
+
+
+
+
+// let myobject ={
+//     firstname: 'rati',
+//     lastname: 'grdzelidze',
+//     Adress: 'tbilisi',
+//     age: 15,
+//     phonenumber: [
+//      { first: 557875544,
+//        second: 555055458
+//      }
+
+//     ]
+// }
+
+// console.log(myobject)
+
+
+
+
+
+
+
+
+// let num = 10
+
+
+// switch (num) {
+//     case 10:
+//         console.log('es cvladi aris 10')
+//      break;
+//     case 11:
+//         console.log('es cvladi aris 11')
+//      break;
+//     case 12:
+//         console.log('es cvladi aris 12')
+//      break;
+//     case 13:
+//         console.log('es cvladi aris 13')
+//      break;
+//     case 14:
+//         console.log('es cvladi aris 14')
+//      break;
+//     default:
+//         console.log('es cvladi aris 15')
+// }
+
+
+
+
+
+
+
+
+
+// let num1 = 10 
+ 
+// if (num1 > 10) {
+//     console.log('10ze metia')
+// }
+
+// else if (num1== 10) {
+//     console.log('10is tolia')
+// }
+
+// else {
+//     console.log('10ze naklebia')
+// }
+
+
+
+let saxeli=['givi', 'mamuka', 'genadi', 'jumberi', 'murtaza']
+saxeli.shift()
+saxeli.push('givi')
+
+if (saxeli [4]=='givi') [
+    console.log('givi')
+]
+
+else if (saxeli[4]=='mamuka') [
+    console.log('mamuka')
+]
+
+else if (saxeli[4]=='genadi') [
+    console.log('genadi')
+]
+
+else if (saxeli[4]=='jumberi') [
+    console.log('jumberi')
+]
+
+else [
+    console.log[murtaza]
+]
+
+
+
+   let num = 10
+
+
+  switch (num) {
+    case 10:
+        console.log('es cvladi aris 10')
+     break;
+    case 11:
+        console.log('es cvladi aris 11')
+     break;
+    case 12:
+        console.log('es cvladi aris 12')
+     break;
+    case 13:
+        console.log('es cvladi aris 13')
+     break;
+    case 14:
+        console.log('es cvladi aris 14')
+     break;
+    default:
+        console.log('es cvladi aris 15')
+}
+
+
